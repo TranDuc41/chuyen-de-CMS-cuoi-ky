26c9c573e45fe0eb2d5ca92b359526e69f096569
TranDuc41 <td07197@gmail.com>
2023-11-19 17:47:26 +0700

    update template blog home and news page

--- wp-content/themes/jobscout/inc/template-functions.php
+++ wp-content/themes/jobscout/inc/template-functions.php
@@ -327,7 +327,7 @@ function jobscout_entry_footer(){
 			}
             
             if( is_front_page() || is_home() || is_search() || is_archive() ){
-                echo '<a href="' . esc_url( get_the_permalink() ) . '" class="readmore-link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.207 8.58"><defs><style>.c{fill:none;stroke:#2ace5e;}</style></defs><g transform="translate(-701.5 -958.173)"><path class="c" d="M-9326.909-9204.917l-3.937,3.937,3.937,3.937" transform="translate(-8613.846 -8238.518) rotate(180)"/><line class="c" x2="15.154" transform="translate(701.5 962.426)"/></g></svg>' . esc_html( $readmore ) . '</a>';    
+                echo '<a href="' . esc_url( get_the_permalink() ) . '" class="readmore-link">' . esc_html( $readmore ) . '</a>';    
             }
 
             if( is_single() ) echo '<div class="entry-footer-right">';
@@ -336,24 +336,24 @@ function jobscout_entry_footer(){
                 jobscout_comment_count();
             }
             
-            if( get_edit_post_link() ){
-                edit_post_link(
-                    sprintf(
-                        wp_kses(
-                            /* translators: %s: Name of current post. Only visible to screen readers */
-                            __( 'Edit <span class="screen-reader-text">%s</span>', 'jobscout' ),
-                            array(
-                                'span' => array(
-                                    'class' => array(),
-                                ),
-                            )
-                        ),
-                        get_the_title()
-                    ),
-                    '<span class="edit-link">',
-                    '</span>'
-                );
-            }
+            // if( get_edit_post_link() ){
+            //     edit_post_link(
+            //         sprintf(
+            //             wp_kses(
+            //                 /* translators: %s: Name of current post. Only visible to screen readers */
+            //                 __( 'Edit <span class="screen-reader-text">%s</span>', 'jobscout' ),
+            //                 array(
+            //                     'span' => array(
+            //                         'class' => array(),
+            //                     ),
+            //                 )
+            //             ),
+            //             get_the_title()
+            //         ),
+            //         '<span class="edit-link">',
+            //         '</span>'
+            //     );
+            // }
             if( is_single() ) echo '</div>';
 		?>
 	</footer><!-- .entry-footer -->
